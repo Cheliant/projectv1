9aa67909ce14b65d3cc04ef1d904bcbddcaec892
Cheliant <123274809+Cheliant@users.noreply.github.com>
2023-01-29 20:02:52 +0700

    Create setting.java

---
+++ setting.java
@@ -0,0 +1,16 @@
+{
+  {
+  "name" : "cheliant"
+  "age" : "18"
+  "department" : "victim"
+  },
+  {
+    "name" : "cheliand"
+  "age" : "20"
+  "department" : "victims"
+  },
+  {
+    "identity" : PerformClick_fingerprint
+    "Key" : "199866372273090245"
+  }
+  }
